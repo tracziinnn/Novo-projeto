0cf774d552e7dbb8655742db29852823bc75ed37
tracziinnn <160507274+tracziinnn@users.noreply.github.com>
2024-08-23 12:31:20 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -39,7 +39,7 @@ export const perguntas = [
                     "Mesmo fugindo da situação, em outro momento você terá que aceitar a realidade",
                     
                 ],
-                proxima: 2,
+                proxima:4,
             },
         ]
     },
@@ -49,7 +49,7 @@ export const perguntas = [
             {
                 texto: " Você confia nelas, e pergunta o que está havendo.",
                 afirmacao: [
-                    ".",
+                    "",
                     "Percebeu que a IA pode ajudar a encontrar informações úteis na internet de forma mais rápida e direcionada.",
                 ],
                 proxima: 3,
@@ -69,7 +69,7 @@ export const perguntas = [
         enunciado: "Elas te explicam oque está acontecendo e você entende que são suas aliadas. Agora que você está junto com elas, e descobre que terá que lutar contra robôs inimigos! ",
         alternativas: [
             {
-                texto: "Você fic.",
+                texto: "Você fica com elas e começa a se preparar para a batalha!",
                 afirmacao: [
                     "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
                     "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
@@ -77,7 +77,7 @@ export const perguntas = [
                 proxima: 4,
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                texto: "Você foge, pois não confia nelas",
                 afirmacao: [
                     "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
                     "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
@@ -87,10 +87,10 @@ export const perguntas = [
         ]
     },
     {
-        enunciado: "Utilizar informações próprias traz um toque pessoal ao nosso trabalho. Agora imagine que você precisa participar de um debate sobre o quanto a IA impacta no trabalho do futuro. Qual posicionamento tomar?",
+        enunciado: "Eles te encontraram e forçaram contato, pois estão em busca de aliados para ajudalos em sua missão",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                texto: "Você fica com elas e começa a se preparar para a batalha!",
                 afirmacao: [
                     "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
                     "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
@@ -98,7 +98,7 @@ export const perguntas = [
                 proxima: 5,
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                texto: "Você foge, pois não confia nelas",
                 afirmacao: [
                     "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
                     "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
@@ -149,24 +149,4 @@ export const perguntas = [
             },
         ]
     },
-    {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
-        alternativas: [
-            {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: [
-                    "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes.",
-                    "Ainda acha que os meios de desenho tradicionais são mais eficazes para a criatividade, por isso vem estimulando pessoas em suas redes sociais a fazer pintura em aquarela."
-                ],
-            },
-            {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: [
-                    "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!",
-                    "Compartilhou artes em redes sociais como forma de ensinar como se comunicar através da arte.",
-                    "Percebeu que muitas pessoas têm dificuldade em expressar suas ideias desenhando e acha que a IA é capaz de empoderar essas pessoas a tirarem ideias do papel."
-                ],
-            },
-        ]
-    }
-];
+    ];
